6dfd32e436794c35950b8290bcb56a8efe2e59b7
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-05-11 11:37:11 +0800

    add font and style header

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,38 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 const App = () => {
   return (
     <Router>
-        <main className="min-h-screen p-12">
-          <header className="mb-6">
+      <main className="min-h-screen p-8 girassol">
+        <header className="flex items-end mb-6">
+          <div className="brand mr-6">
+            <h1 className="text-4xl font-bold">FooBar</h1>
+          </div>
+
+          <div className="flex justify-between items-center">
             <nav className="flex items-center gap-2 text-purple-700 underline">
               <Link to="/">Home</Link>
               <Link to="/about">About</Link>
               <Link to="/contact">Contact</Link>
             </nav>
-          </header>
-          <div className="p-6 bg-white shadow rounded">
-            <Switch>
-              <Route exact path="/">
-                <h2>Home</h2>
-              </Route>
-              <Route exact path="/about">
-                <h2>About</h2>
-              </Route>
-              <Route exact path="/contact">
-                <h2>Contact</h2>
-              </Route>
-            </Switch>
+            <div>
+              <a href="#a">Login</a>
+              <a href="#a" className="py-2 px-3 text-white bg-purple-400 rounded">Register</a>
+            </div>
           </div>
-        </main>
+        </header>
+        <div className="p-6 bg-white shadow rounded">
+          <Switch>
+            <Route exact path="/">
+              <h2>Home</h2>
+            </Route>
+            <Route exact path="/about">
+              <h2>About</h2>
+            </Route>
+            <Route exact path="/contact">
+              <h2>Contact</h2>
+            </Route>
+          </Switch>
+        </div>
+      </main>
     </Router>
   );
 };
