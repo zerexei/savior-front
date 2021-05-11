584cf25545ba8cd824d50e5bd9a8da3c31c33b72
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-05-11 11:29:00 +0800

    setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
 const App = () => {
   return (
-    <main>
-      <h1>Hello World!</h1>
-    </main>
+    <Router>
+        <main className="min-h-screen p-12">
+          <header className="mb-6">
+            <nav className="flex items-center gap-2 text-purple-700 underline">
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contact">Contact</Link>
+            </nav>
+          </header>
+          <div className="p-6 bg-white shadow rounded">
+            <Switch>
+              <Route exact path="/">
+                <h2>Home</h2>
+              </Route>
+              <Route exact path="/about">
+                <h2>About</h2>
+              </Route>
+              <Route exact path="/contact">
+                <h2>Contact</h2>
+              </Route>
+            </Switch>
+          </div>
+        </main>
+    </Router>
   );
 };
 
