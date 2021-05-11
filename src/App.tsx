1d8bef9332357e6dc152f48f715744df6f2f80a5
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-05-11 11:57:10 +0800

    use NavLink for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,59 @@
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink,
+} from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
-      <main className="min-h-screen p-8 girassol">
-        <header className="flex items-end mb-6">
+      <main className="min-h-screen p-8">
+        <header className="flex items-center mb-6">
           <div className="brand mr-6">
             <h1 className="text-4xl font-bold">FooBar</h1>
           </div>
 
-          <div className="flex justify-between items-center">
-            <nav className="flex items-center gap-2 text-purple-700 underline">
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
-              <Link to="/contact">Contact</Link>
+          <div className="flex justify-between items-center w-full">
+            <nav className="flex items-center gap-2 text-gray-700">
+              <NavLink
+                exact
+                activeClassName="text-purple-400"
+                to="/"
+                className="p-2 hover:underline hover:text-purple-400"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                exact
+                activeClassName="text-purple-400"
+                to="/about"
+                className="p-2 hover:underline hover:text-purple-400"
+              >
+                About
+              </NavLink>
+              <NavLink
+                exact
+                activeClassName="text-purple-400"
+                to="/contact"
+                className="p-2 hover:underline hover:text-purple-400"
+              >
+                Contact
+              </NavLink>
             </nav>
-            <div>
-              <a href="#a">Login</a>
-              <a href="#a" className="py-2 px-3 text-white bg-purple-400 rounded">Register</a>
+            <div className="flex items-center gap-5">
+              <a
+                href="#a"
+                className="p-2 hover:underline hover:text-purple-400"
+              >
+                Login
+              </a>
+              <a
+                href="#a"
+                className="py-2 px-3 text-white bg-purple-400 rounded hover:bg-purple-700"
+              >
+                Register
+              </a>
             </div>
           </div>
         </header>
