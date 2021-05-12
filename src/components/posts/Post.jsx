244d721acc3c5717d35bbd81fde49dcca8c912e9
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-05-12 20:53:23 +0800

    add delete post

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,8 +1,9 @@
 // TODO: create a pages dir to separate components
 // import { useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 
 const Post = () => {
+  const history = useHistory();
   const { id } = useParams();
 
   console.log(id);
@@ -14,13 +15,32 @@ const Post = () => {
   //   return () => {};
   // }, [id]);
 
+  const deletePost = async () => {
+    const req = await fetch(`http://127.0.0.1:3001/posts/${id}/delete`, {
+      method: "DELETE",
+    });
+
+    if (req.status === 204) {
+      history.pushState("/posts");
+    }
+
+    console.log(req);
+  };
+
   return (
     <div className="mx-auto w-1/3 p-4 pb-8 bg-white shadow rounded">
       <div className="flex justify-between">
         <h2 className="mb-2 text-2xl font-bold text-purple-700">Foo Bar Baz</h2>
         <div className="text-xs">
-          <Link to={`/posts/${id}/edit`} className="text-blue-400">Edit</Link>
-          <span className="ml-2 text-red-400">Delete</span>
+          <Link to={`/posts/${id}/edit`} className="text-blue-400">
+            Edit
+          </Link>
+          <span
+            onClick={() => deletePost()}
+            className="ml-2 text-red-400 cursor-pointer"
+          >
+            Delete
+          </span>
         </div>
       </div>
       <p className="text-sm tracking-wide">
